87ae02d2e3c2ae62c89980525bbe22d926c18926
Lukasz Lato <lukasz.lato@summer-solutions.com>
2021-04-28 23:13:16 +0200

    favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -53,5 +53,9 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      'vuepress-plugin-sitemap',
+      {hostname: 'https://beeorm.io'}
+    ]
   ]
 }
